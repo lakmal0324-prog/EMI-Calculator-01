0c8143c5916217203224b5ff0633b5e37bc6ff00
lakmal0324-prog <lakmal0324@gmail.com>
2026-08-20 11:47:47 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('emi-cache').then((cache) => cache.addAll(['./', './index.html'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
